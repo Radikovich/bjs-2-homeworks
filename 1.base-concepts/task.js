a8319dfc10e67e70eb37ce62d8a6c5c7ec2fe770
Radikovich <glorfindeal86@yandex.ru>
2022-06-12 22:06:16 +0300

    %comment%

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,44 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  const d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+    return arr;
+  } else if (d > 0) {
+    const x1 = (-b + Math.sqrt(d)) / (2 * a);
+    const x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(Math.round(x1), Math.round(x2));
+    return arr;
+  } else {
+    const x1 = -b / (2 * a);
+    arr.push(Math.round(x1));
+    return arr;
+  }
 }
 
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  let totalAmount = 0;
+  let p = (parseInt(percent) / 12) / 100;
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  if (isNaN(p) || p < 0) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(parseInt(contribution)) || parseInt(contribution) < 0) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(parseInt(amount)) || parseInt(amount) < 0) {
+      return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  } else {
+    let today = new Date();
+    if (today.getFullYear() > date.getFullYear()) {
+      return `Параметр "Cрок ипотеки" содержит неправильное значение "${date}"`;
+    } else {
+      let s = amount - contribution;						 
+      let n = date.getMonth() - today.getMonth() + (12 * (date.getFullYear() - today.getFullYear()));
+      let monthlyFee = s * (p + (p / (((1 + p)^n) - 1)));
+      let totalAmount = (monthlyFee * n).toFixed(2);
+      console.log(totalAmount);
+      return totalAmount;
+    }
+  }
 }
